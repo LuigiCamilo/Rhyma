d646d29aa5d6aa681e75e2170759cfdf7cf19577
rossemil mazo <orianamazo@gmail.com>
2024-03-07 15:44:55 -0400

    rutas

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :courses
+  resources :advances, only: [:create, :update]
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
